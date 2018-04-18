af301f7a48dcd83276655a765b159fb7bb52ec24
liyiwei <826438633@qq.com>
2018-04-18 15:38:41 +0800

    im-vuer

--- docs/api/Dialog.js
+++ docs/api/Dialog.js
@@ -1,27 +1,105 @@
 let api = [{
-  params: 'data',
-  instructions: '数据列表',
-  type: 'Array',
+  params: 'Confirm组件',
+  instructions: '-',
+  type: '-',
   optional: '-',
   default: '-'
 },{
-  params: 'showStatus',
-  instructions: '控制组件显示',
+  params: 'title',
+  instructions: '标题',
+  type: 'String',
+  optional: '-',
+  default: '-'
+},{
+  params: 'content',
+  instructions: '文本',
+  type: 'String',
+  optional: '-',
+  default: '-'
+},{
+  params: 'styleFor',
+  instructions: '样式',
+  type: 'String',
+  optional: 'ios, android',
+  default: '-'
+},{
+  params: 'opts',
+  instructions: '配置',
+  type: 'Array',
+  optional: '参考demo,最好不要超过3个配置项',
+  default: 'ios'
+},{
+  params: 'Toast组件',
+  instructions: '-',
+  type: '-',
+  optional: '-',
+  default: '-'
+},{
+  params: 'content',
+  instructions: '文本',
+  type: 'String',
+  optional: '-',
+  default: '-'
+},{
+  params: 'position',
+  instructions: '位置',
+  type: 'String',
+  optional: 'top, bottom, center',
+  default: 'bottom'
+},{
+  params: 'time',
+  instructions: '显示时间',
+  type: 'Number',
+  optional: '-',
+  default: '3000'
+},{
+  params: 'ActionSheet组件',
+  instructions: '-',
+  type: '-',
+  optional: '-',
+  default: '-'
+},{
+  params: 'hasCancel',
+  instructions: '是否有取消按钮',
   type: 'Boolean',
-  optional: 'true, false',
+  optional: '-',
   default: 'false'
 },{
-  params: '@cancelBtn',
-  instructions: '取消按钮事件',
-  type: 'Function',
+  params: 'opts',
+  instructions: '配置',
+  type: 'Array',
+  optional: '参考demo',
+  default: ''
+},{
+  params: 'Tip组件',
+  instructions: '-',
+  type: '-',
   optional: '-',
   default: '-'
 },{
-  params: '@confirmBtn',
-  instructions: '确定按钮事件',
-  type: 'Function',
+  params: 'txt',
+  instructions: '文本',
+  type: 'String',
   optional: '-',
   default: '-'
+},{
+  params: 'icon',
+  instructions: '图标',
+  type: 'String',
+  optional: 'loading, success, warn, error',
+  default: 'loading'
+},{
+  params: 'tpl',
+  instructions: '自定义模板',
+  type: 'String',
+  optional: '-',
+  default: '-'
+},{
+  params: 'time',
+  instructions: '显示时间',
+  type: 'Number',
+  optional: '-',
+  default: '1800'
 }]
 
 module.exports = api
---
+++ docs/api/Home.js
@@ -0,0 +1,9 @@
+let api = [{
+  params: '404',
+  instructions: '404',
+  type: '404',
+  optional: '404',
+  default: '404'
+}]
+
+module.exports = api
