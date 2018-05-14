6b770e6dc3e625c2287f7add1901e12623c10451
liyiwei <826438633@qq.com>
2018-05-14 09:24:08 +0800

    update

--- src/demo/Progress.vue
+++ src/demo/Progress.vue
@@ -6,20 +6,19 @@
     </im-progress>
     <br>
     <im-progress :progress-width="data2" progress-color="#f00" progress-height="20">
-      <span class="im-progress-start progress-text" slot="endText">100%</span>
     </im-progress>
     <br>
-    <im-progress :progress-width="data3" color="#39ff39" progress-height="5"></im-progress>
-    <br>
-    <button class="btn-progress" @click="upFile">点击上传</button>
+    <im-progress :progress-width="data3" progress-color="#ffbf20" color="#4f00ff" progress-height="5">
+      <span class="im-progress-start progress-text" slot="endText">60%</span>
+    </im-progress>
     <br>
     <im-progress v-show="data4 != 0 && data4 != 100" :progress-width="data4">
       <span class="im-progress-start progress-text" slot="endText">{{data4}}%</span>
     </im-progress>
+    <im-progress progress-color="#f00" :inTop="true" v-show="data5 != 0 && data5 != 100" progress-height="6" :progress-width="data5"></im-progress>
+    <button class="btn-progress" @click="upFile">点击</button>
     <br>
     <button class="btn-progress" @click="top">顶部进度条</button>
-    <br>
-    <im-progress progress-color="#b503ff" :inTop="true" v-show="data5 != 0 && data5 != 100" progress-height="5" :progress-width="data5"></im-progress>
   </div>
 </template>
 
@@ -34,7 +33,7 @@
       return {
         data1: 20,
         data2: 20,
-        data3: 20,
+        data3: 10,
         data4: 0,
         data5: 0
       }
@@ -48,23 +47,34 @@
     mounted() {
       let that = this;
 
-      this.time1 = requestAnimationFrame(function fn1(timestamp) {
-        that.data1++;
-        if (that.data1 > 100) {
-            cancelAnimationFrame(that.time1);
-        } else {
-          requestAnimationFrame(fn1);
-        }
-      });
+      setTimeout(() => {
+        this.time1 = requestAnimationFrame(function fn1(timestamp) {
+          that.data1++;
+          if (that.data1 > 100) {
+              cancelAnimationFrame(that.time1);
+          } else {
+            requestAnimationFrame(fn1);
+          }
+        });
 
-      this.time2 = requestAnimationFrame(function fn2(timestamp) {
-        that.data2++;
-        if (that.data2 >= 80) {
-            cancelAnimationFrame(that.time2);
-        } else {
-          requestAnimationFrame(fn2);
-        }
-      });
+        this.time2 = requestAnimationFrame(function fn2(timestamp) {
+          that.data2++;
+          if (that.data2 >= 80) {
+              cancelAnimationFrame(that.time2);
+          } else {
+            requestAnimationFrame(fn2);
+          }
+        });
+
+        this.time3 = requestAnimationFrame(function fn3(timestamp) {
+          that.data3++;
+          if (that.data3 >= 60) {
+              cancelAnimationFrame(that.time3);
+          } else {
+            requestAnimationFrame(fn3);
+          }
+        });
+      }, 500)
 
     },
     methods: {
