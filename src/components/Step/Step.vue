39f282ca9afa14f2115ecfb75bb5e90cc906d6c8
liyiwei <826438633@qq.com>
2018-09-21 11:13:44 +0800

    update step

--- src/components/Step/Step.vue
+++ src/components/Step/Step.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="im-step">
     <div class="im-step-box">
-      <div class="im-step-box-item" v-for="(v, k) in list" :key="k" :class="current == (k + 1) ? 'on' : ''">
-        <p v-if="v.top" class="im-step-box-item-text">{{v.top}}</p>
+      <!-- <div class="im-step-box-item" v-for="(v, k) in list" :key="k" :class="current == (k + 1) ? 'on' : ''"> -->
+      <div class="im-step-box-item" v-for="(v, k) in list" :key="k">
+        <p v-if="v.top" class="im-step-box-item-text" :style="{color: current == (k + 1) ? activeColor : ''}">{{v.top}}</p>
         <p v-else>&nbsp;</p>
-        <div class="im-step-box-item-status" :style="wh"></div>
-        <p v-if="v.bottom" class="im-step-box-item-text">{{v.bottom}}</p>
+        <div class="im-step-box-item-status" :style="[{backgroundColor: current == (k + 1) ? activeColor : ''}, wh]"></div>
+        <p v-if="v.bottom" class="im-step-box-item-text" :style="{color: current == (k + 1) ? activeColor : ''}">{{v.bottom}}</p>
         <p v-else>&nbsp;</p>
       </div>
     </div>
@@ -28,6 +29,10 @@ export default {
     current: {
       type: [Number, String],
       default: 0
+    },
+    activeColor: {
+      type: String,
+      default: '#00bFFF'
     }
   },
   computed: {
@@ -72,7 +77,7 @@ export default {
           width: 80%;
           height: 1%;
           position: absolute;
-          background-color: #a7a7a7;
+          background-color: @base-color-txt-fuzzy;
           top: 50%;
           transform: translate3d(-63%, -.01px * @baseRem, 0);
         }
@@ -80,23 +85,16 @@ export default {
           /*安卓上面如果px单位带有小数，则会出现圆形变形的情况，使用计算属性动态计算取整*/
           /*width: .2px * @baseRem;*/
           /*height: .2px * @baseRem;*/
-          background-color: #a7a7a7;
+          background-color: @base-color-txt-fuzzy;
           border-radius: 50%;
           position: relative;
+          transition: background-color .3s;
+        }
+        &-text {
+          transition: color .3s;
         }
       }
     }
-  }
-}
-
-.on {
-  .im-step-box-item-text {
-    color: red;
-    transition: all .6s;
-  }
-  .im-step-box-item-status {
-    background-color: red;
-    transition: all .6s;
   }
 }
 
@@ -104,4 +102,9 @@ export default {
   content: '';
   display: none;
 }
+
+/*.im-step-box .on.im-step-box-item:before {
+  content: '';
+  background-color: #f00;
+}*/
 </style>
