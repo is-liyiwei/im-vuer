fd36d0d7ef6c9f43a10baed372318f3e7104b631
liyiwei <826438633@qq.com>
2018-10-18 17:06:41 +0800

    整体效果可以了，但是这个算法其实是有错误的

--- src/demo/PreviewImage.vue
+++ src/demo/PreviewImage.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="demo-previewImage">
     <button @click="openPreviewImage(0)">open</button>
-    <button @click="openPreviewImage(1)">open</button>
-    <button @click="openPreviewImage(2)">open</button>
   </div>
 </template>
 
@@ -11,8 +9,17 @@ export default {
   name: 'previewImage',
   data () {
     return {
-      testImgArr: [{
+      testImgArr1: [{
         src: 'http://img.zcool.cn/community/0117e2571b8b246ac72538120dd8a4.jpg@1280w_1l_2o_100sh.jpg'
+      }, {
+        src: 'http://pic9.nipic.com/20100831/2167235_164504023490_2.jpg'
+      }, {
+        src: 'http://img.zcool.cn/community/0117e2571b8b246ac72538120dd8a4.jpg@1280w_1l_2o_100sh.jpg'
+      }, {
+        src: 'http://pic.58pic.com/58pic/15/70/34/93P58PIC82K_1024.jpg'
+      }],
+      testImgArr2: [{
+        src: 'http://pic.58pic.com/58pic/15/70/34/93P58PIC82K_1024.jpg'
       }, {
         src: 'http://pic9.nipic.com/20100831/2167235_164504023490_2.jpg'
       }, {
@@ -31,8 +38,8 @@ export default {
   methods: {
     openPreviewImage (i) {
       this.$openPreviewImage({
-        imgArr: this.testImgArr,
-        currentIndex: [i]
+        imgArr: this.testImgArr2,
+        currentIndex: i
       })
     }
   }
